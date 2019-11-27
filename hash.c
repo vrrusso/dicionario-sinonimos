278e7ecde40feb2c4c5e7215e09c292fe31967de
Victor R. Russo <vr.russo@usp.br>
2019-11-27 09:44:16 -0300

    inserção na hash concluida

--- hash.c
+++ hash.c
@@ -13,19 +13,79 @@ struct hash_table{
 	int capacity;
 };
 
+/** \brief allocates memory for the table and initialize its values a EMPTY constant.
+ *
+ *	\returns a pointer for the new table.
+ */
 HashTable * createHashTable(){
 	HashTable * h = (HashTable *) malloc(sizeof(HashTable));
 	if(h!=NULL){
 		h->table = (Hash *) malloc(sizeof(Hash)*M);
 		for(int i=0;i<M;i++){
-			strcpy(h->table->native_word,EMPTY);
-			strcpy(h->table->foreign_word,EMPTY);
+			strcpy(h->table[i].native_word,EMPTY);
+			strcpy(h->table[i].foreign_word,EMPTY);
 		}
 		h->capacity=M;
 	}
 	return h;
 }
 
+/** \brief calculates the "weight" of a string define as the sum of the chars
+ *
+ * \param key the string to be evaluate
+ * 
+ * \return the code of the string key
+ *
+ */
+
+int get_string_code(const char * key){
+	int code=0;
+	for(int i=0;i<strlen(key);i++)
+		code+=(key[i]);
+	return code;
+}
+/** \brief computes the index of a string in a hash table, even if theres a collision
+ * 
+ *  \param table[] the hash table itself
+ *  \param key the string used as a key
+ *
+ *  \return the index of the key in the table
+ *
+ */
+int hash_code(Hash table[], const char * key){
+	int integer_key=get_string_code(key);
+	int code=integer_key%M;
+	while(strcmp(table[code].native_word,EMPTY)!=0 && strcmp(table[code].native_word,ERASED)!=0 )
+		code=(code+1)%M;
+	return code;
+}
+
+/** \brief stores a set of two string in the hash table
+ *	
+ *	\param h the hash table complex structure containing the table and its capacity
+ *	\param native_word a string wich is stored in the table
+ *	\param foreign_word
+ *
+ *  \returns the void of existence	 
+ *
+ */
+
+void addInTable(HashTable * h, const char * native_word, const char * foreign_word){
+	if(h->capacity>0){
+		int index = hash_code(h->table,foreign_word);
+		printf("%d",index);
+		strcpy(h->table[index].native_word,native_word);
+		strcpy(h->table[index].foreign_word,foreign_word);
+		h->capacity--;
+		return;
+	}
+	printf("Cannot insert!Full table!\n");
+}
+/** \brief deallocates the space from the table
+ *
+ * 	\param h a pointer for the table itself
+ *
+ */
 void eraseHashTable(HashTable ** h){
 	if((*h)!=NULL){
 		free((*h)->table);
--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 #include "hash.h"
+
+
 /**
  * \brief This function reads  and process an add instruction.
  *	
  */
 void add_word(){
-	char trash[10],native_word[512],foreign_word[512];
+	char trash[TRASH],native_word[MAX_WORD],foreign_word[MAX_WORD];
 	scanf(" %s %s %s",trash,native_word,foreign_word);
 	printf("|%s| |%s| |%s|\n",trash,native_word,foreign_word);
 }
@@ -17,7 +19,7 @@ void add_word(){
  *
  */
 char * read_one_word(){
-	char trash[10];
+	char trash[TRASH];
 	char * foreign_word;
 		scanf(" %s %ms",trash,&(foreign_word));
 	return foreign_word;
@@ -33,15 +35,20 @@ void remove_word(){
 	free(foreign_word);
 }
 /**
- * \brief This function reads  and process a fin instruction.
+ * \brief This function reads  and process a find instruction.
  */
 void find(){
 	char * foreign_word = read_one_word();
 	printf("|%s|\n",foreign_word);
 	free(foreign_word);
 }
+
+
+
 int main(void){
 	HashTable * teste=createHashTable();
+	addInTable(teste,"a","aa");
+	addInTable(teste,"a", "aa");
 	char operation;
 	while(scanf(" %c",&operation) != EOF){
 		if     (operation=='a'||operation=='A')
